25c5741b1d6ed2e40146754013ace77fa3609880
扬帆@Company <yangfan.you@chaosgaming.mobi>
2019-09-12 14:33:33 +0800

    model add

---
+++ www/app.py
@@ -0,0 +1,20 @@
+from aiohttp import web
+import asyncio
+import logging
+logging.basicConfig(level=logging.INFO)
+
+# 定义服务器响应返回为 "Awesome Website"
+
+
+async def index(request):
+    return web.Response(body=b'<h1>Awesome Website</h1>', content_type='text/html')
+
+
+def init():
+    app = web.Application()
+    app.router.add_get('/', index)
+    web.run_app(app, host='127.0.0.1', port=9000)
+
+
+if __name__ == "__main__":
+    init()
